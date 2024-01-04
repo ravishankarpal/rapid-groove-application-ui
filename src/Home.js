c58a92f457d330e8552565808ddeb46f839e45fe
Ravi Shankar <ravishankarpl3@gmail.com>
2024-01-04 18:02:30 +0530

    Setup Home Page

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+const Home = () => {
+  
+      return <Wrapper className='home'>Welcome to Rapid Groove
+       Application Home Page</Wrapper>
+
+  
+};
+const Wrapper = styled.section`
+
+background-color:${({theme}) => theme.colors.bg};
+width: 20rem;
+height: 20rem;
+`;
+
+export default Home
